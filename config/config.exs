6f80f0c850b4303030ddf401eac0c2d9d786c2a2
Karmen <lu.kar@husky.neu.edu>
2019-02-07 18:34:37 -0500

    generating secret at deploy time

--- config/config.exs
+++ config/config.exs
@@ -5,10 +5,23 @@
 # is restricted to this project.
 use Mix.Config
 
+get_secret = fn name ->
+  # Secret generation hack by Nat Tuck for CS4550
+  # This function is dedicated to the public domain.
+  base = Path.expand("~/.config/phx-secrets")
+  File.mkdir_p!(base)
+  path = Path.join(base, name)
+  unless File.exists?(path) do
+    secret = Base.encode16(:crypto.strong_rand_bytes(32))
+    File.write!(path, secret)
+  end
+  String.trim(File.read!(path))
+end
+
 # Configures the endpoint
 config :memory, MemoryWeb.Endpoint,
   url: [host: "localhost"],
-  secret_key_base: "",
+  secret_key_base: get_secret.("key_base");
   render_errors: [view: MemoryWeb.ErrorView, accepts: ~w(html json)],
   pubsub: [name: Memory.PubSub,
            adapter: Phoenix.PubSub.PG2]
